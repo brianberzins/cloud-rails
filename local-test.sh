cac93156d8edd74c986fe0901319ac2505a73cc6
brianberzins <brianberzins@gmail.com>
2022-01-23 11:55:21 -0600

    added local kubernetes test

---
+++ local-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+
+# delete any previous kind cluster
+kind delete cluster || true
+# create a local kind cluster
+kind create cluster --config kubernetes/kind-config.yml
+
+# build the docker image
+docker build --tag cloud-ruby:local .
+# load the docker image locally into kind
+kind load docker-image cloud-ruby:local
+
+# run our our local deployment (including namespace, deployment, service)
+kubectl apply --filename kubernetes/local-deployment.yml
+# wait for our application to be ready
+kubectl wait --namespace cloud-ruby --for=condition=ready pod --selector=app=cloud-ruby --timeout=10s
+
+# test that we can get to the application
+curl --include --silent http://localhost:80/hello-world | grep "200 OK"
